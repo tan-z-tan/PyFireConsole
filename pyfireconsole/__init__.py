738e9ac0c59e6bdb993d819a4ce7eaa12f6f6134
Makoto Tanji <tanji.makoto@gmail.com>
2023-08-03 14:18:37 +0900

    Create console

--- pyfireconsole/__init__.py
+++ pyfireconsole/__init__.py
@@ -1,2 +1,3 @@
 from pyfireconsole.models.firestore_model import FirestoreModel, Collection, DocumentRef
 from pyfireconsole.db.connection import FirestoreConnection
+from pyfireconsole.console.pyfireconsole import PyFireConsole
--- pyfireconsole/console/pyfireconsole.py
+++ pyfireconsole/console/pyfireconsole.py
@@ -3,7 +3,7 @@ from IPython.terminal.embed import embed
 import os
 import sys
 import glob
-import importlib
+import importlib.util
 import inspect
 
 
@@ -12,6 +12,9 @@ class PyFireConsole:
         self.model_dir = model_dir
 
     def run(self, reset_global=False):
+        # Get the caller's global namespace
+        caller_globals = inspect.currentframe().f_back.f_globals
+
         # If a model directory is specified, add it to the system path
         if self.model_dir:
             module_path = os.path.abspath(self.model_dir)
@@ -20,8 +23,19 @@ class PyFireConsole:
 
             # Import all the python files as modules
             for filename in glob.glob(os.path.join(module_path, '*.py')):
-                importlib.import_module(os.path.basename(filename)[:-3])
+                module_name = os.path.splitext(os.path.basename(filename))[0]
+                spec = importlib.util.spec_from_file_location(module_name, filename)
+                module = importlib.util.module_from_spec(spec)
+
+                spec.loader.exec_module(module)
+
+                # Inspect all classes in the module and add them to global namespace
+                for _, obj in inspect.getmembers(module):
+                    # Import iff the object is a class and is not already in the global namespace
+                    if inspect.isclass(obj) and obj.__module__ == module_name and obj.__name__ not in caller_globals:
+                        print(f"Importing {obj.__name__} from {module_name}")
+                        caller_globals[obj.__name__] = obj
 
         # Start the interactive shell
-        user_ns = None if reset_global else inspect.currentframe().f_back.f_globals
-        embed(banner1="\nWelcome to PyFireConsole ====================\n", exit_msg="Bye", user_ns=user_ns, confirm_exit=False)
+        user_ns = None if reset_global else caller_globals
+        embed(banner1="\n==================== Welcome to PyFireConsole ====================\n", exit_msg="Bye", user_ns=user_ns, confirm_exit=False)
