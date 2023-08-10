92fd3743dbd93dc66e2e364fea23f6ecea8e06d5
Makoto Tanji <tanji.makoto@gmail.com>
2023-08-10 22:07:52 +0900

    Version 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfireconsole',
-    version='0.0.7',
+    version='0.0.8',
     author='Makoto Tanji',
     author_email='tanji.makoto@gmail.com',
     description='An interactive console for Firestore based on Python ORM',
